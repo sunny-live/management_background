4efc1bbfa0b8a8e8ce7b04cdbced5fcf0509ddb3
git <1045064247@qq,com>
2021-04-08 17:48:28 +0800

    submit the ones

---
+++ src/main/java/com/example/common/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.example.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 描述:
+ * 作者： xq
+ * 日期： 2020/5/8 13:28
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .addPathPatterns("/page/end/**")
+                .excludePathPatterns("/page/end/login.html", "/page/end/register.html");
+    }
+}
